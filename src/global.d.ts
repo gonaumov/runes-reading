702590f6ece5af582d7571d5b4268763fd60cd90
Georgi Naumov <gonaumov@gmail.com>
2019-06-03 17:14:30 +0300

    Typing the initial state

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,3 +1,21 @@
+interface Rune {
+  rune_id: string
+  rune_name: string
+  rune_image: string
+  rune_meaning: string
+}
+
+interface Spread {
+  spread_id: string
+  spread_name: string
+  spread_description: string
+  spread_runes_count: number
+}
+
+
+
 interface State {
-    
+  runes: Array<Rune>
+  spreads: Array<Spread>
+  selected_spread: 5 | 4 | 3 | 1 | null
 }
