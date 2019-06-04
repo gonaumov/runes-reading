76b5a623db190e22172a11b00ed9cefe773f4b46
Georgi Naumov <gonaumov@gmail.com>
2019-06-04 18:06:13 +0300

    Updates

---
+++ precache-manifest.1ef4c92cb63bb6349ae9edf53837612b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdb8aad1ab4a0c788cb03fe889656cca",
+    "url": "/runes-reading/index.html"
+  },
+  {
+    "revision": "64fd9c034af9ef759fa8",
+    "url": "/runes-reading/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "64fd9c034af9ef759fa8",
+    "url": "/runes-reading/static/js/2.1aacbdc3.chunk.js"
+  },
+  {
+    "revision": "5abff1f12e30c4ad744d",
+    "url": "/runes-reading/static/js/main.fc8f435b.chunk.js"
+  },
+  {
+    "revision": "d66895bff7e87d19430e",
+    "url": "/runes-reading/static/js/runtime~main.e36b83f6.js"
+  }
+]);
